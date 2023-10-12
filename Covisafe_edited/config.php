990583cb97e2521fa45b8a25aa0e1480e7d960b0
AhmedKamruddin <98866395+AhmedKamruddin@users.noreply.github.com>
2023-10-12 10:08:04 +0530

    Delete Covisafe_edited directory

--- Covisafe_edited/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DBSERVER','localhost');
-define('DBUSERNAME','covisafeDBA');
-define('DBPASSWORD','covisafe123');
-define('DBNAME','Covisafe');
-
-$db=mysqli_connect(DBSERVER,DBUSERNAME,DBPASSWORD,DBNAME);
-
-?>
--- Covisafe_edited/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-if( isset($_SESSION["covisafeDBA"]) && $_SESSION["covisafeDBA"]===true)
-{
-    header("location: welcome.php");
-}
-?>
